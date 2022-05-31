2b70051d8c9a3ffbaedaa0102132d00499d3b06b
Huu Duc DO <58935702+Dustin-Do@users.noreply.github.com>
2022-05-31 16:23:55 +0200

    Add files via upload

--- rfml/nn/model/factory.py
+++ rfml/nn/model/factory.py
@@ -1,50 +1,98 @@
-"""Simplistic factory pattern for config defined swapping of architecture used.
+"""Simplistic factory pattern for swapping of datasets.
 """
 __author__ = "Bryse Flowers <brysef@vt.edu>"
 
+# External Includes
+from typing import Tuple
+
 # Internal Includes
-from .cldnn import CLDNN
-from .cnn import CNN
-from .resnet_CT import resnet18, resnetx
-from .conv5 import TinyConv
-from .vgg16 import VGG16
-def build_model(model_name: str, input_samples: int, n_classes: int):
-    """Factory method for dynamic creation of multiple neural architectures.
+from .converters import load_RML201610A_dataset, load_RML201610B_dataset
+from .dataset import Dataset
+from .encoder import Encoder
+
+
+def build_dataset(
+    dataset_name: str, test_pct: float = 0.3, val_pct: float = 0.05, path: str = None
+) -> Tuple[Dataset, Dataset, Dataset, Encoder]:
+    """Opinionated factory method that allows easy loading of different datasets.
+
+    This method makes an assumption about the labels to use for each dataset -- if you
+    need more extensive control then you can call the underlying method directly.
 
     Args:
-        model_name (str): The name of the model to build.  Currently
-                          supported models are:
-                            - "CNN"
-                            - "CLDNN"
-        input_samples (int): Number of complex input samples to the model.
-        n_classes (int): Number of output classes the model should predict.
+        dataset_name (str): Name of the dataset to load.  Currently supported values
+                            are:
+                            - RML2016.10A
+                            - RML2016.10B
+        test_pct (float, optional): Percentage of the entire Dataset that should be
+                                    withheld as a test set. Defaults to 0.3.
+        val_pct (float, optional): Percentage of the non-testing Dataset that should be
+                                   split out to use for validation in an early stopping
+                                   procedure. Defaults to 0.05.
+        path (str, optional): If provided, this is directly passed to the dataset
+                              converters so that they do not download the dataset from
+                              the internet (a costly operation) if you have already
+                              downloaded it yourself. Defaults to None.
+
+    Raises:
+        ValueError: If test_pct or val_pct are not between 0 and 1 (non-inclusive).
+        ValueError: If the dataset_name is unknown.
 
     Returns:
-        Model: The built model described by the provided parameters
+        Tuple[Dataset, Dataset, Dataset, Encoder]: train, validation, test, encoder
     """
-    if input_samples <= 0:
+    if test_pct >= 1.0 or test_pct <= 0.0:
         raise ValueError(
-            "The model must take in at least one sample for the input, not {}".format(
-                input_samples
-            )
+            "Testing percentage must be in the open set between (0-1).  "
+            "Not {}.".format(test_pct)
         )
-    if n_classes <= 2:
+    if val_pct >= 1.0 or val_pct <= 0.0:
         raise ValueError(
-            "The models built by this method are for multi-class classification and "
-            "therefore must have at least 3 classes to predict from, not {}".format(
-                n_classes
-            )
+            "Validation percentage must be in the open set between (0-1).  "
+            "Not {}.".format(test_pct)
         )
 
-    if model_name.upper() == "CNN":
-        return CNN(input_samples=input_samples, n_classes=n_classes)
-    elif model_name.upper() == "CLDNN":
-        return CLDNN(input_samples=input_samples, n_classes=n_classes)
-    elif model_name.upper() == "resnet18":
-        return resnet18(num_classes=11)
-    elif model_name.upper() == "conv5":
-        return resnetx(num_classes=11)
-    elif model_name.upper() == "VGG16":
-        return VGG16(input_samples=input_samples, n_classes=n_classes)
+    if dataset_name.upper() == "RML2016.10A":
+        dataset = load_RML201610A_dataset(path=path)
+        le = Encoder(
+            [
+                "WBFM",
+                "AM-DSB",
+                "AM-SSB",
+                "CPFSK",
+                "GFSK",
+                "PAM4",
+                "BPSK",
+                "QPSK",
+                "8PSK",
+                "QAM16",
+                "QAM64",
+            ],
+            label_name="Modulation",
+        )
+        on = ["Modulation", "SNR"]
+    elif dataset_name.upper() == "RML2016.10B":
+        dataset = load_RML201610B_dataset(path=path)
+        le = Encoder(
+            [
+                "WBFM",
+                "AM-DSB",
+                "CPFSK",
+                "GFSK",
+                "PAM4",
+                "BPSK",
+                "QPSK",
+                "8PSK",
+                "QAM16",
+                "QAM64",
+            ],
+            label_name="Modulation",
+        )
+        on = ["Modulation", "SNR"]
     else:
-        raise ValueError("Unknown neural network architecture ({})".format(model_name))
+        raise ValueError("Unknown dataset ({})".format(dataset_name))
+
+    train, test = dataset.split(frac=test_pct, on=on)
+    train, val = dataset.split(frac=val_pct, on=on)
+
+    return train, val, test, le
